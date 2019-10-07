31d06bef3654675cae0f8fe474d606ea95c6cbdc
NorbertasL <nlapenas@gmail.com>
2019-10-07 16:16:04 +0100

    Added a btnGlow() function

--- assets/js/followMe.js
+++ assets/js/followMe.js
@@ -1,6 +1,28 @@
+
 //Making sure document has loaded
 $(document).ready(function(){
+  //Button click CallBack functions
   $("#game_btn_red").on("click", function(){
-    console.log("RED pressed")
+    btnGlow(this);
+  });
+  $("#game_btn_green").on("click", function(){
+    btnGlow(this);
+  });
+  $("#game_btn_yellow").on("click", function(){
+    btnGlow(this);
+  });
+  $("#game_btn_blue").on("click", function(){
+    btnGlow(this);
   });
 });
+
+
+//Makes button glow
+function btnGlow(btn){
+  //DEBUG
+  console.log("Cicked on "+btn.id)
+  let lastColor = $(btn).attr("fill");
+  $(btn).animate({svgFill:"white"}, 200);
+  $(btn).animate({svgFill:lastColor}, 100);
+
+}
