ca5846ed3938e45db9bc81fd6d2f38c54eebbe57
NorbertasL <nlapenas@gmail.com>
2019-10-08 21:48:26 +0100

    Added a way for the computer to play the sequance.

--- assets/js/followMe.js
+++ assets/js/followMe.js
@@ -1,61 +1,64 @@
-let gameRunning = false;
-let gameLvl = 1;
+let gameLvl = 5;
 let userTurn = false;
-let needPatter = true;
 let sequance = [];
+let computerSequance = [];
+
+let timeStamp;
+
+//ID's
+const  RED_ID = "#game_btn_red";
+const  GREEN_ID = "#game_btn_green";
+const  YELLOW_ID = "#game_btn_yellow";
+const  BLUE_ID = "#game_btn_blue";
 //Making sure document has loaded
 $(document).ready(function(){
   $("#game_btn_start").on("click", function(){
     console.log("Start btn clicked");
-    resetGame();
-    startGame();
+    timeStamp = Date.now();
+    requestAnimationFrame(gameLoop);
   });
 
   //Button click CallBack functions
   $("#game_btn_red").on("click", function(){
-    btnGlow(this);
+    pressButton(1)
+    console.log(Date.now())
   });
   $("#game_btn_green").on("click", function(){
-    btnGlow(this);
+    pressButton(2)
   });
   $("#game_btn_yellow").on("click", function(){
-    btnGlow(this);
+    pressButton(3)
   });
   $("#game_btn_blue").on("click", function(){
-    btnGlow(this);
+    pressButton(4)
   });
 });
 
 
 //Makes button glow
-function btnGlow(btn){
+function btnGlow(btnID){
   //DEBUG
-  console.log("Cicked on "+btn.id)
-  let lastColor = $(btn).attr("fill");
-  $(btn).animate({svgFill:"white"}, 100);
-  $(btn).animate({svgFill:lastColor}, 100);
+  console.log("Cicked on "+btnID);
+  let lastColor = $(btnID).attr("fill");
+  $(btnID).animate({svgFill:"white"}, 100);
+  $(btnID).animate({svgFill:lastColor}, 100);
 }
 
-//Game start method
-function startGame(){
-  //Fliping the game state
-  gameRunning = !gameRunning;
-  gameLoop();
-
-}
 //Main game loop
 function gameLoop(){
-  console.log("Game is running:"+gameRunning);
-  if(needPatter){
-    sequance.push(getRandomBtn());
-    needPatter = false;
+  console.log("Game loop called");
+  if(sequance.length == 0){
+    for(i=sequance.length; i<=gameLvl; i++){
+      sequance.push(getRandomBtn());
+    }
     console.log("sequance:"+sequance);
   }
-
-  if(gameRunning){
-    //This basicly is a while loop, but more stable
-    requestAnimationFrame(gameLoop);
+  if(!userTurn){
+    playPattern();
   }
+
+  //Calling iteslef again
+  requestAnimationFrame(gameLoop);
 }
 function getRandomBtn(){
   return Math.floor((Math.random() * 4) + 1);
@@ -66,7 +69,34 @@ function resetGame(){
   needPatter = true;
   sequance = [];
 }
+function playPattern(){
+  if(computerSequance.length == 0){
+    computerSequance = sequance;
+  }
+  let ts = Date.now();
+  //if 1 sec has pased since last timestamp
+  if(ts-timeStamp > 1000){
+    timeStamp = ts;
+    pressButton(computerSequance.pop());
+  }
+}
 
+function pressButton(button){
+  switch(button) {
+  case 1://Red
+    btnGlow(RED_ID)
+    break;
+  case 2://Green
+    btnGlow(GREEN_ID)
+    break;
+  case 3://Yello
+    btnGlow(YELLOW_ID)
+    break;
+  case 4://Blue
+    btnGlow(BLUE_ID)
+    break;
+  }
+}
 //Thank you https://www.sitepoint.com/delay-sleep-pause-wait/
 function sleep(milliseconds) {
   var start = new Date().getTime();
