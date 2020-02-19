b025df2e4870981def8bfedc9a6ce0494cf73557
NorbertasL <nlapenas@gmail.com>
2020-02-19 14:05:07 +0000

    Normalised the coordinate system

--- assets/js/FollowMeGame/Console.js
+++ assets/js/FollowMeGame/Console.js
@@ -1,23 +1,30 @@
+let gameCenter, radius, mainBtn, gameWidth, gameHeight;
 export default class Console {
   constructor(canvasWidth, canvasHeight){
-    this.gameWidth = canvasWidth;
-    this.gameHeight = canvasHeight;
-    this.radius = this.gameHeight/2 - 20;
+    gameWidth = canvasWidth;
+    gameHeight = canvasHeight;
+    gameCenter = {
+      x:canvasWidth/2,
+      y:canvasHeight/2
+    }
+    radius = gameHeight/2 - 20;
 
     //Loading btn images
-    this.mainBtn = {
+    mainBtn = {
       start_normal:new Image(),
       start_pressed:new Image(),
       restart_normal:new Image(),
       restart_pressed:new Image()
     }
-    this.mainBtn.start_normal.src = "assets/js/FollowMeGame/img/start_btn.png";
-    this.mainBtn.start_pressed.src = "assets/js/FollowMeGame/img/start_btn_pressed.png";
-    this.mainBtn.restart_normal.src = "assets/js/FollowMeGame/img/restart_btb.png";
-    this.mainBtn.restart_pressed.src = "assets/js/FollowMeGame/img/restart_btn_pressed.png";
+    mainBtn.start_normal.src = "assets/js/FollowMeGame/img/start_btn.png";
+    mainBtn.start_pressed.src = "assets/js/FollowMeGame/img/start_btn_pressed.png";
+    mainBtn.restart_normal.src = "assets/js/FollowMeGame/img/restart_btb.png";
+    mainBtn.restart_pressed.src = "assets/js/FollowMeGame/img/restart_btn_pressed.png";
 
     //Innitialising btn states
-    this.mainBtn = this.mainBtn.start_normal;
+    mainBtn = mainBtn.start_normal;
+
+
 
 
 
@@ -25,11 +32,11 @@ export default class Console {
 
   draw(ctx){
 
-    ctx.clearRect(0, 0, this.gameWidth, this.gameHeight);
+    ctx.clearRect(0, 0, gameWidth, gameHeight);
 
     //Outer console ring
     ctx.beginPath();
-    ctx.arc(this.gameWidth/2, this.gameHeight/2, this.radius, 0, 2 * Math.PI);
+    ctx.arc(gameCenter.x, gameCenter.y, radius, 0, 2 * Math.PI);
     ctx.strokeStyle = "black";
     ctx.lineWidth = 10;
     ctx.closePath();
@@ -39,8 +46,8 @@ export default class Console {
     //Green
     ctx.beginPath();
     //We have to add this, otherwise, it will fill the minimum area needed to fill the arc
-    ctx.moveTo(this.gameWidth/2, this.gameHeight/2);
-    ctx.arc(this.gameWidth/2, this.gameHeight/2, this.radius-10, 1.5*Math.PI, 0);
+    ctx.moveTo(gameCenter.x, gameCenter.y);
+    ctx.arc(gameCenter.x, gameCenter.y, radius-10, 1.5*Math.PI, 0);
     ctx.fillStyle = "green";
     ctx.closePath();
     ctx.fill();
@@ -48,8 +55,8 @@ export default class Console {
     //Yellow
     ctx.beginPath();
     //We have to add this, otherwise, it will fill the minimum area needed to fill the arc
-    ctx.moveTo(this.gameWidth/2, this.gameHeight/2);
-    ctx.arc(this.gameWidth/2, this.gameHeight/2, this.radius-10, 0, 0.5*Math.PI);
+    ctx.moveTo(gameCenter.x, gameCenter.y);
+    ctx.arc(gameCenter.x, gameCenter.y, radius-10, 0, 0.5*Math.PI);
     ctx.fillStyle = "yellow";
     ctx.closePath();
     ctx.fill();
@@ -57,8 +64,8 @@ export default class Console {
     //Blue
     ctx.beginPath();
     //We have to add this, otherwise, it will fill the minimum area needed to fill the arc
-    ctx.moveTo(this.gameWidth/2, this.gameHeight/2);
-    ctx.arc(this.gameWidth/2, this.gameHeight/2, this.radius-10, 0.5*Math.PI, Math.PI );
+    ctx.moveTo(gameCenter.x, gameCenter.y);
+    ctx.arc(gameCenter.x, gameCenter.y, radius-10, 0.5*Math.PI, Math.PI );
     ctx.fillStyle = "blue";
     ctx.closePath();
     ctx.fill();
@@ -66,17 +73,17 @@ export default class Console {
     //Red
     ctx.beginPath();
     //We have to add this, otherwise, it will fill the minimum area needed to fill the arc
-    ctx.moveTo(this.gameWidth/2, this.gameHeight/2);
-    ctx.arc(this.gameWidth/2, this.gameHeight/2, this.radius-10, Math.PI, 1.5*Math.PI );
+    ctx.moveTo(gameCenter.x, gameCenter.y);
+    ctx.arc(gameCenter.x, gameCenter.y, radius-10, Math.PI, 1.5*Math.PI );
     ctx.fillStyle = "red";
     ctx.closePath();
     ctx.fill();
 
     //Inner(manu) Console
     let mainConsolePos = {
-      x:this.gameWidth/2,
-      y:this.gameHeight/2,
-      radius: this.radius/2
+      x:gameCenter.x,
+      y:gameCenter.y,
+      radius: radius/2
     }
     ctx.beginPath();
     ctx.arc(mainConsolePos.x, mainConsolePos.y, mainConsolePos.radius, 0, 2 * Math.PI);
@@ -85,14 +92,24 @@ export default class Console {
     ctx.fill();
 
     //Manu buttons
-    ctx.drawImage(this.mainBtn, mainConsolePos.x-50, mainConsolePos.y+50, 100, 40);
+    ctx.drawImage(mainBtn, mainConsolePos.x-50, mainConsolePos.y+50, 100, 40);
 
 
 
 
   }
 
-  update(dt){
+  update(pos){
+    //Checking if we are inside the console
+
+    let dx = pos.x - gameCenter.x;
+    let dy = pos.y - gameCenter.y;
+    let dist = Math.sqrt(dx*dx+dy*dy)
+    if(dist < radius){
+      debug("Inside main console", "Console.update")
+    }else{
+      debug("Outside main console", "Console.update")
+    }
 
   }
 
--- assets/js/FollowMeGame/input.js
+++ assets/js/FollowMeGame/input.js
@@ -1,11 +1,29 @@
+import Console from "/assets/js/FollowMeGame/console.js"
+
+let canvas;
+let gameConsole
+
 export default class Input{
-  constructor(canvas){
-    this.canvas = canvas;
-    this.canvas.addEventListener("mousedown", this.onMouseDown, false)
+  constructor(can, console){
+    canvas = can;
+    gameConsole = console;
+    canvas.addEventListener("mousedown", this.onMouseDown, false)
   }
 
-  onMouseDown(){
-    debug("Mouse is down", "Input")
+  onMouseDown(event){
+    var normaliseCoordinate = function(event){
+      let bounds = canvas.getBoundingClientRect();
+      let scaleX = canvas.width / bounds.width;
+      let scaleY = canvas.height / bounds.height;
+      return {
+        x:(event.clientX-bounds.left)*scaleX,
+        y:(event.clientY-bounds.top)*scaleY
+      }
+    }
+    var pos = normaliseCoordinate(event);
+
+    gameConsole.update(pos)
+
   }
 
 }
